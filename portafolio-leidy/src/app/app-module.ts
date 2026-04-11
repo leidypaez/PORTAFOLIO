829c73096d6c42078f0022c209cecd70469ac9a2
leidypaez <leidy16paez@gmail.com>
2026-04-10 23:49:20 -0500

    corrigiendo el routing models

--- portafolio-leidy/src/app/app-module.ts
+++ portafolio-leidy/src/app/app-module.ts
@@ -1,5 +1,6 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { NgModule, provideBrowserGlobalErrorListeners, } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
@@ -7,6 +8,15 @@ import { Navbar } from './components/navbar/navbar';
 import { Hero } from './components/hero/hero';
 import { About } from './components/about/about';
 import { Footer } from './components/footer/footer';
+import { Proyectos } from './components/proyectos/proyectos';
+import { Home } from './components/home/home';
+
+
+const routes: Routes = [
+  { path: '', component: Home }, 
+  { path: 'proyectos', component: Proyectos },
+  { path: 'about', component: About }
+];
 
 @NgModule({
   declarations: [
@@ -14,11 +24,13 @@ import { Footer } from './components/footer/footer';
     Navbar,
     Hero,
     About,
-    Footer
+    Footer,
+    Proyectos,
+    Home
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
---
+++ portafolio-leidy/src/app/components/proyectos/proyectos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lgp-proyectos',
+  standalone: false,
+  templateUrl: './proyectos.html',
+  styleUrl: './proyectos.css',
+})
+export class Proyectos {
+
+}
